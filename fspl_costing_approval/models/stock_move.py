8dac40bb6f206e8e3961d20ed7f1d8ae81eda84e
Krupa Patel <kup@odoo.com>
2025-07-24 12:59:27 +0530

    [IMP] fspl_costing_approval: added last price and total amount on moves
    - added custom fields on stock move, picking and on operation types
    - based on boolean value configurred on opertion type, triggerred
      the values of last purchase price and total amount while
      validating transfers.
    - view extension for field display and visibility conditionally
    
    task-4940258

---
+++ fspl_costing_approval/models/stock_move.py
@@ -0,0 +1,15 @@
+from odoo import api, fields, models
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    last_purchase_price = fields.Float(string='Last Purchase Price', readonly=True, store=True)
+    total_amount = fields.Float(string='Total Amount', store=True, readonly=True)
+
+    def _get_last_purchase_price_and_total_amount(self):
+        for move in self:
+            if move.picking_id and move.picking_id.picking_type_id.is_last_purchase_price:
+                move.last_purchase_price = move.product_id.last_price
+            if move.picking_id and move.picking_id.picking_type_id.is_total_amount:
+                move.total_amount = move.last_purchase_price * move.quantity
