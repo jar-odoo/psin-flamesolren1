fecc5ee5272a0206868d2fdf51ad3eac0fab8ad0
Jaydeep Barot <jar@odoo.com>
2025-07-30 17:59:16 +0530

    18.0 fspl project task 4852712 jish (#12) (#13)
    * [ADD] fspl_costing_approval: sales margin costing optional products
    
    - margin & discount master setup – Configurable by user and city
    - Product enhancements – master price, last Price (auto-updated)
    - quotation optional products line enhancements – required Qty,
      auto price fetching, tax handling, read-only behavior
    - costing logic – material & expense cost split based on
      product type, DC Capacity input
    - base amount & final sales amount computation – including
      bifurcation logic (70% / 30%)
    - json field introduce for optional products tax details
    - approval flow – auto creation of approval request based on
      discount thresholds, with read-only state transitions
    - approval Smart buttons, hiding confirm/mail buttons conditionally
    
    task-4852586
    
    * [IMP] removed pyc files
    
    * [IMP] fspl_costing_approval: changed misc
    
    * [IMP] fspl_costing_approval: made additional changes
    
    * [IMP] fspl_costing_approval: invisible attribute discarded
    
    * [IMP] fspl_costing_approval: added last price and total amount on moves
    
    - added custom fields on stock move, picking and on operation types
    - based on boolean value configurred on opertion type, triggerred
      the values of last purchase price and total amount while
      validating transfers.
    - view extension for field display and visibility conditionally
    
    task-4940258
    
    * [IMP] fspl_costing_approval: enhanced code logic
    
    * [ADD] flame_solren_project_custom: implement task planning and dependency management
    
    - Added planned and actual scheduling mechanism for project tasks based on sales
     order confirmation date and estimated task duration i.e.Number of Days.
    - Introduced 'creation_plan' flag to allow certain tasks to be scheduled and
    started independently.
    - Implemented finish-to-start logic for task dependencies using 'blocked_by'
    relationships.
    - Automatically set actual start and end dates when task state changes to
    'In Progress' or 'Done'.
    - Enforced sequential task execution: tasks blocked by others remain in
    'Waiting' until dependencies are completed.
    - On sales order cancellation, related projects and their tasks are also set to
    'Cancelled' with appropriate stage updates.
    - Extended project and task models to support enhanced scheduling, tracking, and
     dependency management linked to sales flow.
    - Also displayed all the custom fields in form view and list view of task.
    - Created a flag in project stages to identify which stage is cancelled, in
    order to appropriately set the project to the cancelled stage when the linked
    sales order is cancelled.
    
    task- 4852712
    
    * Optimized the code
    
    * Removed unnecessary conditions from the compute method of delay/early based on planned/actual dates
    
    ---------
    
    Co-authored-by: Krupa Patel <kup@odoo.com>
    Co-authored-by: jish-odoo <jish@odoo.com>

---
+++ flame_solren_project_custom/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Flame Solren - Project Task Custom',
+    'version': '1.0',
+    'description': """
+                    The planned scheduled start and end dates of each task are calculated based on the sales order 
+                    confirmation date and the number of days specified for that task. When a task's state is changed
+                    to In Progress or Done, its actual start and end dates—along with those of any dependent tasks 
+                    are updated accordingly. This allows the system to determine whether a task was completed early
+                    or delayed by comparing the actual and planned dates.Tasks listed in the Blocked By field are 
+                    executed sequentially; a parent task cannot begin until all its dependent (child) tasks are 
+                    marked as Done, otherwise it will remain in the Waiting state. However, tasks with the Creation
+                    Plan enabled are independent and can start without waiting for other tasks to be completed.
+                """,
+    'odoo_task_id' : '4852712',
+    'category': 'Customizations',
+    'author': 'Odoo PS-IN',
+    'depends': [
+        'project',
+        'sale_project',
+    ],
+    'data': [
+        "views/project_task_views.xml",
+        "views/project_project_stage_views.xml",
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'flame_solren_project_custom/static/src/project_task_state_selection.js',
+        ],
+    },
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
---
+++ flame_solren_project_custom/models/sale_order.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import models, api
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    def action_confirm(self):
+        res = super().action_confirm()
+        for order in self:
+            tasks = self.env['project.task'].search([
+                ('sale_line_id', 'in', order.order_line.ids),
+            ])
+            if tasks:
+                tasks._auto_chain_blockers()
+                tasks._schedule_tasks_finish_to_start(order.date_order)
+                for task in tasks:
+                    if not task.depend_on_ids:
+                        task.state = '05_to_do'
+                        task.actual_start_date = False
+                        task.actual_end_date = False
+        return res
+    
+    def _action_cancel(self):
+        res = super()._action_cancel()
+        cancel_stage = self.env['project.project.stage'].search([('is_cancelled_stage', '=', True)], limit=1)
+        if cancel_stage:
+            self.project_ids.write({'stage_id': cancel_stage.id})
+        self.tasks_ids.filtered(lambda t: t.state not in ['1_done', '1_canceled']).write({'state': '1_canceled'})
+        return res
