59c29f15004c97987189af1586b69e5660ea4ccb
Jaydeep Barot <jar@odoo.com>
2025-08-04 15:54:59 +0530

    Odoo.sh rebase and merge (#17)
    * [FIX][4979128] fspl_add_optional_product : Add multiple optional products. (#16)
    
    - Improved code.
    
    Co-authored-by: vkas-odoo <vkas@odoo.com>
    
    * [IMP] fspl_add_optional_product: improve code.
    
    ---------
    
    Co-authored-by: vkas-odoo <vkas@odoo.com>

--- fspl_add_optional_product/models/sale_order_option.py
+++ fspl_add_optional_product/models/sale_order_option.py
@@ -6,15 +6,7 @@ class SaleOrderOption(models.Model):
 
     is_selected = fields.Boolean(string="Select")
 
-    def button_add_to_order(self):
-        existing_products = self.order_id.order_line.mapped('product_id')
-
-        if self.product_id in existing_products:
-            return
-        result = self.add_option_to_order()
-        self.is_selected =False
-        result.write({
-            'product_uom_qty': self.required_qty,
-            'price_unit': 0.00,
-            'tax_id': False,
-        })
+    def _get_values_to_add_to_order(self):
+        result = super()._get_values_to_add_to_order()
+        result.update({'price_unit': 0.00, 'product_uom_qty': self.required_qty})
+        return result
