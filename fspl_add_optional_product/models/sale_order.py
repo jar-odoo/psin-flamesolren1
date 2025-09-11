45377ab09a261dbfff556cc896e8f09f501d721e
Krupa Patel <kup@odoo.com>
2025-09-11 15:28:18 +0530

    [IMP] fspl_add_optional_product: added project co-ordinator column (#34)
    * [IMP] fspl_add_optional_product: added project co-ordinator column
    
    - added new field in sale order optional products
    - fetch the required qty's value on that field while confirming the sale order
    - passed that fields updated value on add to cart.
    - view extension and attributes appliance
    
    task-5078822
    
    * [IMP] fspl_add_optional_product: button visibility condition change
    
    * [IMP] fspl_add_optional_product: misc changes

--- fspl_add_optional_product/models/sale_order.py
+++ fspl_add_optional_product/models/sale_order.py
@@ -12,10 +12,16 @@ class SaleOrder(models.Model):
         # Filter remove all option line which is present in order line.
         selected_options = self.sale_order_option_ids.filtered(lambda opt: opt.is_selected and not opt.is_present)
         for option in selected_options:
-            option.button_add_to_order()
+            option.with_context(fspl_add_options=True).button_add_to_order()
         self.select_all_options = False
 
     @api.onchange('select_all_options')
     def _onchange_select_all_options(self):
         for line in self.sale_order_option_ids:
             line.is_selected = self.select_all_options
+
+    def action_confirm(self):
+        for order in self:
+            for option in order.sale_order_option_ids:
+                option.write({'project_coordinator_qty': option.required_qty})
+        return super().action_confirm()
--- fspl_add_optional_product/models/sale_order_option.py
+++ fspl_add_optional_product/models/sale_order_option.py
@@ -7,8 +7,18 @@ class SaleOrderOption(models.Model):
     _inherit = 'sale.order.option'
 
     is_selected = fields.Boolean(string="Select")
+    project_coordinator_qty = fields.Float(
+        string="Project Co-ordinator Qty",
+        help="Defaults to Required Qty but can be adjusted manually by user."
+    )
 
     def _get_values_to_add_to_order(self):
         result = super()._get_values_to_add_to_order()
-        result.update({'price_unit': 0.00, 'product_uom_qty': self.required_qty})
+        result.update({'price_unit': 0.00, 'product_uom_qty': self.project_coordinator_qty})
         return result
+
+    def _can_be_edited_on_portal(self):
+        self.ensure_one()
+        if self._context.get('fspl_add_options', False):
+            return True
+        return super()._can_be_edited_on_portal()
