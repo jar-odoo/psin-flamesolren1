ffdb4b9313a17324829acd77f7d334abe04d4bed
Krupa Patel <kup@odoo.com>
2025-09-12 17:02:40 +0530

    18.0 fspl project co ordinator option line kup (#37)
    * [IMP] fspl_add_optional_product: added project co-ordinator column (#34)
    
    * [IMP] fspl_add_optional_product: added project co-ordinator column
    
    - added new field in sale order optional products
    - fetch the required qty's value on that field while confirming the sale order
    - passed that fields updated value on add to cart.
    - view extension and attributes appliance
    
    task-5078822
    
    * [IMP] fspl_add_optional_product: button visibility condition change
    
    * [IMP] fspl_add_optional_product: misc changes
    
    * [FIX] fspl_add_optional_product: context defining
    
    * [IMP] fspl_costing_approval: auto fetch required qty from quantity field.

--- fspl_add_optional_product/models/sale_order_option.py
+++ fspl_add_optional_product/models/sale_order_option.py
@@ -7,8 +7,12 @@ class SaleOrderOption(models.Model):
     _inherit = 'sale.order.option'
 
     is_selected = fields.Boolean(string="Select")
+    project_coordinator_qty = fields.Float(
+        string="Project Co-ordinator Qty",
+        help="Defaults to Required Qty but can be adjusted manually by user."
+    )
 
     def _get_values_to_add_to_order(self):
         result = super()._get_values_to_add_to_order()
-        result.update({'price_unit': 0.00, 'product_uom_qty': self.required_qty})
+        result.update({'price_unit': 0.00, 'product_uom_qty': self.project_coordinator_qty})
         return result
